ebad218920639107dbb0182a13d7279e93b66488
gauravxcode <gauravmohore111@gmail.com>
2024-01-18 15:51:11 +0530

    updated scraping logic

--- src/main/java/practice/JsonOps.java
+++ src/main/java/practice/JsonOps.java
@@ -4,10 +4,18 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v120.io.IO;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class JsonOps {
 
@@ -38,7 +46,37 @@ public class JsonOps {
         }
     }
 
+    public static void generateFile() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonFileName = String.format("%s-%o.json", "location", System.currentTimeMillis());
+        String jsonFilePath = String.format("%s\\src\\main\\resources\\saved\\%s", System.getProperty("user.dir"), jsonFileName);
+        System.out.println(jsonFileName);
+
+        ArrayNode stateArray = objectMapper.createArrayNode();
+        WebDriver stateDriver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+        try {
+            stateDriver.get("https://shuru.co.in/news");
+            stateDriver.findElements(By.cssSelector("a[title]")).forEach(stateElement -> {
+                ObjectNode stateObject = objectMapper.createObjectNode();
+                stateObject.put("locName", stateElement.getText());
+                stateObject.put("locLevel", "STATE");
+                stateObject.put("locUrl", stateElement.getAttribute("href"));
+                stateArray.add(stateObject);
+            });
+            ObjectNode jsonRoot = objectMapper.createObjectNode();
+            jsonRoot.set("locList", stateArray);
+            try {
+                objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(jsonFilePath), jsonRoot);
+            } catch (IOException err) {
+                System.out.println(err.getMessage());
+            }
+        } finally {
+            stateDriver.quit();
+        }
+    }
+
     public static void main(String[] args) throws JsonProcessingException {
         /* Test Methods */
+        generateFile();
     }
 }
--- src/main/java/practice/RandomActions.java
+++ src/main/java/practice/RandomActions.java
@@ -1,38 +1,59 @@
 package practice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RandomActions {
+
     public static void main(String[] args) {
-        print();
 
-    }
+        ObjectMapper objectMapper = new ObjectMapper();
 
-    static void print() {
-        WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
-        try {
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-            driver.get("https://shuru.co.in/");
-            List<WebElement> footerSections = driver.findElements(By.xpath("//footer/div/div[2]/div"));
-
-            for (int i = 0; i < footerSections.size(); i++) {
-                String secTitle = footerSections.get(i).findElement(By.xpath("./div[1]")).getText();
-                List<WebElement> linkList = footerSections.get(i).findElements(By.xpath("./div[2]/a"));
-                for (int j = 0; j < linkList.size(); j++) {
-
-                    System.out.println(String.format("%d.%d", i + 1, j + 1));
-                }
+        File jsonFile = new File("output.json");
+        if (!jsonFile.exists()) {
+            try {
+                jsonFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } finally {
-            driver.quit();
         }
+
+
+//        WebDriver driverRoot = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+        WebDriver driverRoot = new ChromeDriver();
+        driverRoot.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Map<String, String> stateUrlInfo = new LinkedHashMap<>();
+        try {
+            driverRoot.get("http://shuru.co.in./news");
+            driverRoot.findElements(By.cssSelector("a[title]")).forEach(element -> stateUrlInfo.put(element.getText(), element.getAttribute("href")));
+        } finally {
+            driverRoot.quit();
+        }
+        System.out.println(stateUrlInfo.size());
+//        stateUrlInfo.forEach((k, v) -> System.out.println(k + " -> " + v));
+        stateUrlInfo.forEach((sName, sUrl) -> {
+//            WebDriver driverNode = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+            WebDriver driverNode = new ChromeDriver();
+            try {
+                driverNode.get(sUrl);
+                Map<String, String> districtUrlInfo = new LinkedHashMap<>();
+                driverNode.findElements(By.cssSelector("div[class=list-items]>a")).forEach((element -> districtUrlInfo.put(element.getText(), element.getAttribute("href"))));
+                districtUrlInfo.forEach((k, v) -> System.out.println(k + " --> " + v));
+            } finally {
+                driverNode.quit();
+            }
+        });
     }
 }
